e1577f7b3bdb873cf958c6adedaa28fbfbe46e07
hedlundaren <simhe813@student.liu.se>
2017-11-30 17:11:55 +0100

    gridlist init

--- src/components/MainSection/index.tsx
+++ src/components/MainSection/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import { CSSProperties } from 'react'
 import * as style from './style.css'
-import { portfolioItems, IPortfolioItem } from '../../portfolio';
+import { portfolioItems, IPortfolioItem } from '../../portfolio'
+import { GridList, GridListTile } from 'material-ui/GridList'
 
 export namespace MainSection {
   export interface Props {
@@ -13,18 +14,29 @@ export namespace MainSection {
 export class MainSection extends React.Component<MainSection.Props, MainSection.State> {
 
   render() {
-    const porfolioComponents = portfolioItems
-      .map((portfolioItem: IPortfolioItem) => (
-        <div>
-          {portfolioItem.header}
-          <img src={portfolioItem.imagePaths[0]} />
-        </div>
-      ))
+    // const porfolioComponents = portfolioItems
+    //   .map((portfolioItem: IPortfolioItem) => (
+    //     <div>
+    //       {portfolioItem.header}
+    //       <img src={portfolioItem.imagePaths[0]} />
+    //     </div>
+    //   ))
+      
+
 
     return (
-      <div>
-        {porfolioComponents}
-      </div>
+
+      <GridList cellHeight={160} cols={3}>
+        <div > hej</div>
+      </GridList>
     )
   }
 }
+
+
+//   {tileData.map(tile => (
+    //     <GridListTile key={tile.img} cols={tile.cols || 1}>
+    //       <img src={tile.img} alt={tile.title} />
+    //     </GridListTile>
+    //   ))}
+    
