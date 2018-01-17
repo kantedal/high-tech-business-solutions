074777550f1e1b2349c41d88bb5dc3408f0177f1
kantedal <kantedal@gmail.com>
2018-01-17 09:10:23 +0100

    Testing

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,9 +15,17 @@ export namespace Header {
   export interface State {}
 }
 
+const StyledHeaderAnimation = keyframes`
+  from { filter: hue-rotate(0deg) grayscale(50%); }
+  to { filter: hue-rotate(360deg) grayscale(50%); }
+`
+
 export const StyledHeaderDiv: any = styled.div`
   position: relative;
-  background: linear-gradient(rgba(132, 112, 206, 1.0) 30%, rgba(126, 75, 192, 0.94) 90%);
+  // background: linear-gradient(rgba(132, 112, 206, 1.0) 30%, rgba(126, 75, 192, 0.94) 90%);
+  background: linear-gradient(rgb(216, 216, 216) 30%,rgb(255, 255, 255) 90%);
+  // animation: ${StyledHeaderAnimation} 10s linear infinite;
+  filter: brightness(100%);
   box-shadow: 0 0 15px rgba(0, 0, 0, 0.7);
   margin-bottom: 20px;
   height: ${({ isActive, scrollY }: any) => (window.innerHeight) + 'px'};
@@ -39,6 +47,13 @@ const StyledArrow = styled.i`
   color: #fff;
 `
 
+const StyledText = styled.div`
+  text-fill-color: transparent;
+  background-image: -webkit-linear-gradient(92deg, #f35626, #feab3a);
+  background-clip: text;
+  animation: ${StyledHeaderAnimation} 10s linear infinite;
+`
+
 export class Header extends React.Component<Header.Props, Header.State> {
   render() {
     const { isActive, scrollY } = this.props
@@ -46,41 +61,43 @@ export class Header extends React.Component<Header.Props, Header.State> {
     return (
       <StyledHeaderDiv isActive={isActive}>
         {/* <div className={style.top} /> */}
-        <Grid className={style.presentationGrid} fluid={true}>
-          <Row center='xs' className={style.presentationRow}>
-            <Col xs={12} sm={12} md={12} lg={9}>
-              <Row>
-                <Col xs={0} sm={6}>
-                  <p className={style.presentation}>
-                    Welcome to our portfolio. We are two friends who enjoy coding and creativity. 
-                    Currently we are looking for work. If you like what you see here and are looking for two programmers, please contact
-                    us.
-                  </p>
-                </Col>
-                <Col xs={6} sm={3}>
-                  <PresentationBox
-                    name={'Simon Hedlund'}
-                    imgUrl={'./images/simon.jpg'}
-                    linkedInUrl={'https://www.linkedin.com/in/simon-hedlund-a1a656128/'}
-                    emailUrl={'sermonhedlund@gmail.com'}
-                    githubUrl={'https://github.com/Hedlundaren'}
-                    websiteUrl={'http://simonhedlund.github.io'}
-                  />
-                </Col>
-                <Col xs={6} sm={3}>
-                  <PresentationBox
-                    name={'Filip Kantedal'}
-                    imgUrl={'./images/filip.jpg'}
-                    linkedInUrl={'https://www.linkedin.com/in/filip-kantedal-33b84240/'}
-                    emailUrl={'kantedal@gmail.com'}
-                    githubUrl={'https://github.com/kantedal'}
-                    websiteUrl={'http://kantedal.se'}
-                  />
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Grid>
+        <StyledText>
+          <Grid className={style.presentationGrid} fluid={true}>
+            <Row center='xs' className={style.presentationRow}>
+              <Col xs={12} sm={12} md={12} lg={9}>
+                <Row>
+                  <Col xs={0} sm={6}>
+                    <p className={style.presentation}>
+                      Welcome to our portfolio. We are two friends who enjoy coding and creativity. 
+                      Currently we are looking for work. If you like what you see here and are looking for two programmers, please contact
+                      us.
+                    </p>
+                  </Col>
+                  <Col xs={6} sm={3}>
+                    <PresentationBox
+                      name={'Simon Hedlund'}
+                      imgUrl={'./images/simon.jpg'}
+                      linkedInUrl={'https://www.linkedin.com/in/simon-hedlund-a1a656128/'}
+                      emailUrl={'sermonhedlund@gmail.com'}
+                      githubUrl={'https://github.com/Hedlundaren'}
+                      websiteUrl={'http://simonhedlund.github.io'}
+                    />
+                  </Col>
+                  <Col xs={6} sm={3}>
+                    <PresentationBox
+                      name={'Filip Kantedal'}
+                      imgUrl={'./images/filip.jpg'}
+                      linkedInUrl={'https://www.linkedin.com/in/filip-kantedal-33b84240/'}
+                      emailUrl={'kantedal@gmail.com'}
+                      githubUrl={'https://github.com/kantedal'}
+                      websiteUrl={'http://kantedal.se'}
+                    />
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Grid>
+        </StyledText>        
         <div className={style.arrowRow}>
           <Link 
             activeClass='active'
