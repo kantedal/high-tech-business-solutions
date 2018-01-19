b14571216466678413d5a98f9aa3a09092d185c8
Filip Kantedal <kantedal@gmail.com>
2018-01-19 01:24:05 +0100

    gh-pages

--- src/actions/app.ts
+++
@@ -1,9 +0,0 @@
-import { createAction } from 'redux-actions'
-import * as Actions from '../constants/actions'
-import {push, replace} from 'react-router-redux'
-import { IPortfolioItem } from '../portfolio';
-
-export const backToMenu = () => replace('/')
-export const openPortfolioItem = createAction<IPortfolioItem>(Actions.OPEN_PORTFOLIO_ITEM)
-export const closePortfolioItem = createAction(Actions.CLOSE_PORTFOLIO_ITEM)
-export const filterByPortfolioCategory = createAction<string>(Actions.FILTER_PORTFOLIO_BY)
